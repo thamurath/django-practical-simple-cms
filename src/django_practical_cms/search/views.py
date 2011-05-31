5f172ad73d59bd60ff3bb31bc000a50b4c5f1a21
thamurath <thamurath@gmail.com>
2011-06-01 00:59:45 +0200

    Improve search.
    Now if someone try to go directly to /search/ get a search box page

--- src/django_practical_cms/search/views.py
+++ src/django_practical_cms/search/views.py
@@ -1,14 +1,17 @@
 
-from django.http import HttpResponse
-from django.template import loader, Context
+
 from django.contrib.flatpages.models import FlatPage
 from django.shortcuts import render_to_response
 
 def search(request):
-    query = request.GET['q']
+    query = request.GET.get('q', '')
+    results = []
+    if query:
+        results = FlatPage.objects.filter(content__icontains=query)
+        
     return render_to_response('search/search.html',
                               {'query':query,
-                               'results' : FlatPage.objects.filter(content__icontains=query)
+                               'results' : results ,
                                }
                             )
     #===========================================================================
